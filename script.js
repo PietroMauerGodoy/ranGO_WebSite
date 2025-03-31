000551356fa82f63db0ca2a9146ac0383f33b3ed
PietroGodoy <157855554+PietroMauerGodoy@users.noreply.github.com>
2025-03-31 13:19:02 -0300

    Site ranGO! - Base

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*fechar e abrir o popup*/
 document.addEventListener("DOMContentLoaded", function() {
     var popup = document.getElementById("contact-popup");
     var contactBtn = document.getElementById("contact-btn");
@@ -19,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-/*aceitar apenas números em "telefone" */
 function openPopup() {
     document.getElementById("myPopup").style.display = "block";
 }
@@ -37,73 +35,6 @@ function openPopup() {
     var popup = document.getElementById("myPopup");
     popup.classList.add("show");
 }
-
-/*abrir link externo */
-
-document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('header nav ul li a[href^="#"]');
-
-    links.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
-});
-
-/*telas do produto */
-
-const images = document.querySelectorAll('.fade-img');
-const texts = [
-    "Login",
-    "Inicial (home)",
-    "Dashboard",
-    "Estoque",
-    "Fornecedor",
-    "Pedidos",
-    "Opções"
-];
-const textElement = document.getElementById('text');
-let currentImageIndex = 0;
-
-function showNextImage() {
-    
-    images[currentImageIndex].classList.remove('show');
-    
-   
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-
-    images[currentImageIndex].classList.add('show');
-
-    textElement.style.opacity = 0; 
-    setTimeout(() => {
-        textElement.textContent = texts[currentImageIndex];
-        textElement.style.opacity = 1; 
-    }, 500); 
-}
-
-
-setInterval(showNextImage, 3500);
-
-
-function goToPaymentIniciante(planName, price) {
-    // Redireciona para a página de pagamento com parâmetros de query string (opcional)
-    window.location.href = `pixInicial.html?plan=${planName}&price=${price}`;
-}
-
-
-function goToPaymentPage(planName, price) {
-    // Redireciona para a página de pagamento com parâmetros de query string (opcional)
-    window.location.href = `pixPro.html?plan=${planName}&price=${price}`;
-}
-
-
-
 
 
 
